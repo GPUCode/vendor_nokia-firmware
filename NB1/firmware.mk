818253df3885a47b8b2ee210ff87433f403fa682
emufan4568 <geoster3d@gmail.com>
2021-12-22 16:47:38 +0200

    NB1: Initial 516A firmware + A1N keymaster
    * This repo contains the most up-to-date firmware for NB1, the
    October 2020 SP with the addition of the A1N keymaster which allows
    proper use of the hardware backed QTI 3.0 HAL

---
+++ NB1/firmware.mk
@@ -0,0 +1,34 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),NB1)
+
+RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
+$(foreach f, $(notdir $(RADIO_FILES)), \
+    $(call add-radio-file,radio/$(f)))
+
+FIRMWARE_IMAGES := \
+    abl \
+    bluetooth \
+    cda \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    hidden \
+    hyp \
+    keymaster \
+    mdtp \
+    mdtpsecapp \
+    modem \
+    nvdef \
+    persist \
+    pmic \
+    rpm \
+    splash \
+    systeminfo \
+    tz \
+    xbl
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
+
+endif
